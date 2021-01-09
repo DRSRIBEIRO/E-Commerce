694c757364c0839e381c9e33338dc8090675e9ff
Daniel Ribeiro <drsantosribeiro@gmail.com>
2021-01-09 17:50:31 -0300

    Configuracao do Projeto

---
+++ ECommerce/ECommerce/Models/EcommerceContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ECommerce.Models
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
